b1ec5d687d20f9f93cd519263f6e145112c5da99
Aashu-Dubey <Aashu-Dubey@users.noreply.github.com>
2023-01-28 13:52:54 +0000

    deploy: 7b80681d4bf26d0891af248001ad5dfafe2065c1

---
+++ 8461.b8fd88949960a628.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[8461],{8461:(r,t,e)=>{e.r(t),e.d(t,{SafeAreaWeb:()=>_});var n=e(5861),a=e(7423);class _ extends a.Uw{getStatusBarHeight(){return(0,n.Z)(function*(){return{height:0}})()}getSafeAreaInsets(){return(0,n.Z)(function*(){return{top:0,bottom:0,left:0,right:0}})()}}}}]);
