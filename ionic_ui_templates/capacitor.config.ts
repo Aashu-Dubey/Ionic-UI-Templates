519a509da8951dcb4600416a9a95211ae8494f2b
Ashutosh <asutosh.dubey97@gmail.com>
2023-05-14 23:28:36 +0530

    chore: upgrade to Capacitor v5 and latest Ionic version

---
+++ ionic_ui_templates/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.templates',
+  appName: 'ionic_ui_templates',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
